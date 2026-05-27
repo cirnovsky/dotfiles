ba3d3554ee20784272b974881614bb53bca10215
cirnovsky <cirnovskyv@gmail.com>
2026-05-27 09:24:34 +0800

    [emacs] remove .emacs.rc

--- .emacs.custom.el
+++ .emacs.custom.el
@@ -4,9 +4,7 @@ k
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(company gruvbox-theme magit markdown-mode move-text multiple-cursors
-	     rust-mode typescript-mode uv-mode xclip)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
